3f937c34ddb7c6c4661d290ed0e6fdb1977fc678
Diego Acevedo <diexterminator@hotmail.cl>
2020-12-02 21:28:26 -0300

    terminado la interfaz del login :D

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -20,15 +20,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Container(
-                      height: 400,
+                      height: 220,
                       child: Stack(
                           children: <Widget>[
                             Positioned(
                               top: -25,
-                              height: 300,
+                              height: 275,
                               width: width,
                               child: FadeAnimation(1, Container(
-                                margin: EdgeInsets.symmetric(vertical: 30, horizontal: 35),
+                                margin: EdgeInsets.symmetric(vertical: 35, horizontal: 40),
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(20),
                                     border: Border.all(
@@ -45,7 +45,77 @@ class _LoginScreenState extends State<LoginScreen> {
                             ),
                           ]
                       )
-                  )
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 35),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                      FadeAnimation(1.5, Text("Login", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25),)),
+                      SizedBox(height: 7,),
+                      FadeAnimation(1.7, Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(3),
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(
+                                  color: Colors.grey
+                                ))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Username",
+                                  hintStyle: TextStyle(color: Colors.grey)
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(3),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(
+                                      color: Colors.grey
+                                  ))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(color: Colors.grey)
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      )),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Center(child: Text("Forgot password?", style: TextStyle(color: Colors.black)),),
+                          SizedBox(
+                          height: 16,
+                          ),
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.symmetric(horizontal: 50),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(70),
+                          color: Colors.teal[900]
+                        ),
+                        child: Center(
+                          child: Text("Login", style: TextStyle(color: Colors.white),),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(child: Text("Create Account", style: TextStyle(color: Colors.black)),),
+                      ],
+                  ),)
                 ]
             )
         ));
