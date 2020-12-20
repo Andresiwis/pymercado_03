4f4405f5ab9b9e5ce4f863ee99af82ad5b68e72d
Diego Acevedo <diexterminator@hotmail.cl>
2020-12-19 23:03:16 -0300

    diseño inicial del perfil

--- lib/profile.dart
+++ lib/profile.dart
@@ -7,7 +7,7 @@ class Profile extends StatelessWidget {
         backgroundColor: Colors.tealAccent,
         body: SingleChildScrollView(
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -15,10 +15,46 @@ class Profile extends StatelessWidget {
                     Icon(
                       Icons.store,
                       size: 75,
-                    )
+                    ),
+                    Text("NOMBRE", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
                   ],
                 ),
-              ]),
-        ));
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text("categoria"),
+                      Column(
+                        children: <Widget>[
+                          Text("dirección"),
+                          Text("numero de telefono")
+                        ],
+                      )
+                    ]
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Center(
+                    child: Text("Descripcion")
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: RaisedButton(
+                        child: Center(
+                          child: Text("Chatea aquí"),
+                        ),
+                      onPressed: () {}
+                      ),
+                      ),
+              ]
+          ),
+        )
+    );
   }
 }
